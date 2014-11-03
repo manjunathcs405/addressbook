25ab2a37ba2ae1db4e40569fe884f982d465f955
manjunatha.s <manjunatha.s@lge.com>
2014-11-03 12:25:11 +0530

    intial commit for AddressBook Managemenet

---
+++ Person.cpp
@@ -0,0 +1,23 @@
+#include "Person.h"
+
+Person::Person():
+    name(""),
+    mobileNo(""),
+    emailId(""),
+    address(""),
+    groups("")
+{
+}
+Person::~Person()
+{
+
+}
+bool Person::fillDetails()
+{
+    return true;
+}
+bool Person::getDetails();
+{
+    return true;
+}
+
