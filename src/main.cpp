035bb6258c2e9ac87cb38108258b50c7bb9da93c
manjunatha.s <manjunatha.s@lge.com>
2014-11-04 17:33:53 +0530

    assert function is added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include<assert.h>
+#include "Phone_AddressBook.h"
 
 int main()
 {
     std::cout<<"ENtering Main";
+    AddressBook *base_ptr = new Phone_AddressBook();
+    bool sucess = base_ptr->addContact();
+    assert(sucess);
     std::cout<<"Exiting Main";
     return(0);
 }
